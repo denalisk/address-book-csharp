fd28bfad94fad0fb62c6df6e18c686a96996a028
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-10 09:23:46 -0800

    create the full definition for the SaveContact and DeleteContact methods in the Book class

--- Objects/Book.cs
+++ Objects/Book.cs
@@ -12,16 +12,25 @@ namespace AddressBook.Objects
             _contactList.Add(newContact);
             _contactDictionary[newContact.GetName()] = newContact;
         }
-        public static void DeleteContact(Contact newContact)
+        public static bool DeleteContact(Contact newContact)
         {
-            bool success;
+            bool success_list = false;
+            bool success_dictionary = false;
             for (int index = 0; index < _contactList.Count; index++)
             {
                 if (_contactList[index].GetId() == newContact.GetId())
                 {
-
+                    _contactList.RemoveAt(index);
+                    success_list = true;
+                    break;
                 }
             }
+            if (_contactDictionary.ContainsKey(newContact.GetId()))
+            {
+                _contactDictionary.Remove(newContact.GetId());
+                success_dictionary = true;
+            }
+            return (success_dictionary && success_list)
         }
 
     }
