51342ecc204014bad26c1dfb02d88d553d86d9cb
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-10 09:42:39 -0800

    add a POST route to the HomeModule grabbing contact info input and adding it to the Book list and dictionary. Also adds AddressBook.Objects to the using at the top of HomeModules, it was ommitted before

--- HomeModules.cs
+++ HomeModules.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using AddressBook.Objects;
 
 namespace AddressBook
 {
@@ -14,6 +15,18 @@ namespace AddressBook
         Get["/add"] = _ =>
         {
             return View["forms.cshtml"];
+        };
+        Post["/new-contact"] = _ =>
+        {
+            string newName = Request.Form["new-name"];
+            int newPhoneNumber = Request.Form["new-phone-number"];
+            string newStreetAddress = Request.Form["new-street-address"];
+            string newCity = Request.Form["new-city"];
+            string newState = Request.Form["new-state"];
+            Address newAddress = new Address(newStreetAddress, newCity, newState);
+            Contact newContact = new Contact(newName, newPhoneNumber, newAddress);
+            Book.SaveContact(newContact);
+            return View["contact.cshtml", newContact];
         };
       // GETs and POSTs go HERE = _ =>
     }
